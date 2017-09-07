1037729851feb14e5e32c1cd19205204ed9dbcb5
Andrey Shynkarenko <andreyshynkarenko@Andreys-MacBook-Air.local>
2017-09-07 13:01:06 +0300

    finishing

--- solution/js/app.js
+++ solution/js/app.js
@@ -3,51 +3,56 @@
 
   var apiKey = 'e238eb49738ff4c9b8abac9b526cba73',
     imagesPerPage = 24,
-    term;
+    term = '',
+    imagesSearch = new FlickrImagesSearch(apiKey,imagesPerPage),
+    displayedPages = 0;
 
-  var imagesSearch = new FlickrImagesSearch(apiKey,imagesPerPage),
-    $photosContainer = $('.photos-container'),
+  var $photosContainer = $('.photos-container'),
     $searchInput = $('form input[type="text"]'),
     $submitFormButton = $('form input[type="submit"]'),
     $searchForm = $('form'),
     $historyContainer = $('.history-container'),
-    $historyTable = $('table');
+    $historyTable = $('table'),
+    $showMoreButton = $('button.show-more');
 
   $searchForm.submit(function(event){
     event.preventDefault();
     term = $searchInput.val();
     $photosContainer.html('');
     imagesSearch.getFlickrPhotos(term,1,disableSearch,enableSearch,displaySearchResults,displayError);
+    displayedPages = 0;
   })
 
   function disableSearch(){
     $submitFormButton.attr('disabled','');
     $submitFormButton.val('Loading...');
-    $historyTable.attr('disabled','');
   }
 
   function enableSearch(){
     $submitFormButton.removeAttr('disabled');
     $submitFormButton.val('Search');
-    $historyTable.removeAttr('disabled');
   }
 
   function displaySearchResults(pagesNumber, resultsNumber, photos){
 
     var searchTime = new Date();
-    displayPhotos(pagesNumber, photos);
+    displayPhotos(photos);
     addHistory(term, searchTime, resultsNumber, $historyTable);
-
+    displayShowMoreButton(pagesNumber);
   }
 
-  function displayPhotos(pagesNumber, photos){
+  function displayPhotos(photos){
     if (!photos.length){
       $photosContainer.append('<div class="message">Nothing Is Found</div>');
+      return;
     }
     photos.forEach(function(photo){
-      var photoHTML = '<div class="photo-container"><img src="'+photo.smallURL+'"></div>';
+      var photoHTML = '<div class="photo-container"><a href="'+photo.largeURL+'" target="blank">'+
+        '<img src="'+photo.smallURL+'" alt="'+photo.title+'" title="'+photo.title+'">'+
+        '</a></div>';
       $photosContainer.append(photoHTML);
     })
+    displayedPages++;
   }
 
   function addHistory(term, time, resultsNumber){
@@ -56,7 +61,7 @@
       $historyContainer.html('');
       var historyContentHTML = '<table><tr><th>Search Term</th>'+
         '<th>Time Of Search</th><th>Number Of Search Results</th></tr></table>'+
-        '<button>Clear Search History</button>';
+        '<button class="alert">Clear Search History</button>';
       $historyContainer.html(historyContentHTML);
       $historyContainer.find('button').click(function(){
         $historyContainer.html('');
@@ -70,8 +75,34 @@
 
     $historyTable.find('tr:last').click(function() {
         $searchInput.val(term);
-        $searchForm.submit();
+        $submitFormButton.click();
       })
+  }
+
+  function displayShowMoreButton(pagesNumber){
+    if (pagesNumber > displayedPages){
+      $showMoreButton.css('display','block');
+      $showMoreButton.unbind().click(function(){
+        imagesSearch.getFlickrPhotos(term,displayedPages+1,disableShowMore,enableShowMore,displayNextPage,displayError)
+    });
+    } else {
+      $showMoreButton.css('display','none');
+    }
+  }
+
+  function disableShowMore(){
+    $showMoreButton.attr('disabled','');
+    $showMoreButton.html('Loading...');
+  }
+
+  function enableShowMore(){
+    $showMoreButton.removeAttr('disabled');
+    $showMoreButton.html('Show More');
+  }
+
+  function displayNextPage(pagesNumber, resultsNumber, photos){
+      displayPhotos(photos);
+      displayShowMoreButton(pagesNumber);
   }
 
   function displayError(errorText){
